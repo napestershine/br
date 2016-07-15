eb08ffbc7b28383ffe6456ec93633e72675014c9
wmcpc <raj4mkursela@gmail.com>
2016-07-15 10:17:10 +0530

    CRUD of Slider entity

---
+++ src/Brooter/AdminBundle/Controller/SliderController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Brooter\AdminBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Brooter\AdminBundle\Entity\Slider;
+use Brooter\AdminBundle\Form\SliderType;
+
+/**
+ * Slider controller.
+ *
+ */
+class SliderController extends Controller
+{
+    /**
+     * Lists all Slider entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $sliders = $em->getRepository('BrooterAdminBundle:Slider')->findAll();
+
+        return $this->render('slider/index.html.twig', array(
+            'sliders' => $sliders,
+        ));
+    }
+
+    /**
+     * Creates a new Slider entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $slider = new Slider();
+        $form = $this->createForm('Brooter\AdminBundle\Form\SliderType', $slider);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($slider);
+            $em->flush();
+
+            return $this->redirectToRoute('slider_show', array('id' => $slider->getId()));
+        }
+
+        return $this->render('slider/new.html.twig', array(
+            'slider' => $slider,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Slider entity.
+     *
+     */
+    public function showAction(Slider $slider)
+    {
+        $deleteForm = $this->createDeleteForm($slider);
+
+        return $this->render('slider/show.html.twig', array(
+            'slider' => $slider,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Slider entity.
+     *
+     */
+    public function editAction(Request $request, Slider $slider)
+    {
+        $deleteForm = $this->createDeleteForm($slider);
+        $editForm = $this->createForm('Brooter\AdminBundle\Form\SliderType', $slider);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($slider);
+            $em->flush();
+
+            return $this->redirectToRoute('slider_edit', array('id' => $slider->getId()));
+        }
+
+        return $this->render('slider/edit.html.twig', array(
+            'slider' => $slider,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Slider entity.
+     *
+     */
+    public function deleteAction(Request $request, Slider $slider)
+    {
+        $form = $this->createDeleteForm($slider);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($slider);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('slider_index');
+    }
+
+    /**
+     * Creates a form to delete a Slider entity.
+     *
+     * @param Slider $slider The Slider entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Slider $slider)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('slider_delete', array('id' => $slider->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/Brooter/AdminBundle/Form/SliderType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Brooter\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SliderType extends AbstractType
+{
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Brooter\AdminBundle\Entity\Slider'
+        ));
+    }
+}
