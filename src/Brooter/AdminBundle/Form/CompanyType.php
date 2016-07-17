417160e25afc61fdc643ee0b9cd4d175eb6bc218
beshambher <bishamberc>
2016-07-17 18:07:51 +0530

    fixed half of admin panel

--- src/Brooter/AdminBundle/Form/CompanyType.php
+++ src/Brooter/AdminBundle/Form/CompanyType.php
@@ -13,21 +13,21 @@ class CompanyType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('field1', TextType::class, array('attr'=>['class' => 'col-md-6']))
-            ->add('field2', TextType::class, array('attr'=>['class' => 'col-md-6']))
-            ->add('field3', TextType::class, array('attr'=>['class' => 'col-md-6']))
-            ->add('field4', TextType::class, array('attr'=>['class' => 'col-md-6']))
-            ->add('field5', TextType::class, array('attr'=>['class' => 'col-md-6']))
-            ->add('field6', TextType::class, array('attr'=>['class' => 'col-md-6']))
-            ->add('field7', TextType::class, array('attr'=>['class' => 'col-md-6']))
-            ->add('field8', TextType::class, array('attr'=>['class' => 'col-md-6']))
-            ->add('field9', TextType::class, array('attr'=>['class' => 'col-md-6']))
-            ->add('field10', TextType::class, array('attr'=>['class' => 'col-md-6']))
-            ->add('field11', TextAreaType::class, array('attr'=>['class' => 'col-md-12', 'rows'=>'6']))
-            ->add('field12', TextAreaType::class, array('attr'=>['class' => 'col-md-6']))
-            ->add('field13', TextAreaType::class, array('attr'=>['class' => 'col-md-6']))
-            ->add('field14', TextAreaType::class, array('attr'=>['class' => 'col-md-6']))
-            ->add('field15', TextAreaType::class, array('attr'=>['class' => 'col-md-6']));
+            ->add('field1', TextType::class, array('attr'=>['class' => 'form-control']))
+            ->add('field2', TextType::class, array('attr'=>['class' => 'form-control']))
+            ->add('field3', TextType::class, array('attr'=>['class' => 'form-control']))
+            ->add('field4', TextType::class, array('attr'=>['class' => 'form-control']))
+            ->add('field5', TextType::class, array('attr'=>['class' => 'form-control']))
+            ->add('field6', TextType::class, array('attr'=>['class' => 'form-control']))
+            ->add('field7', TextType::class, array('attr'=>['class' => 'form-control']))
+            ->add('field8', TextType::class, array('attr'=>['class' => 'form-control']))
+            ->add('field9', TextType::class, array('attr'=>['class' => 'form-control']))
+            ->add('field10', TextType::class, array('attr'=>['class' => 'form-control']))
+            ->add('field11', TextAreaType::class, array('attr'=>['class' => 'form-control', 'rows'=>'6']))
+            ->add('field12', TextAreaType::class, array('attr'=>['class' => 'form-control']))
+            ->add('field13', TextAreaType::class, array('attr'=>['class' => 'form-control']))
+            ->add('field14', TextAreaType::class, array('attr'=>['class' => 'form-control']))
+            ->add('field15', TextAreaType::class, array('attr'=>['class' => 'form-control']));
     }
 
     public function configureOptions(OptionsResolver $resolver)
