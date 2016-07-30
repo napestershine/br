101862ce34aad114cd1903317ba797c883e9b186
wmcpc <raj4mkursela@gmail.com>
2016-07-30 18:28:44 +0530

    pushed 30-7-2016 06:29PM

--- src/Brooter/AdminBundle/Entity/PropSubCate.php
+++ src/Brooter/AdminBundle/Entity/PropSubCate.php
@@ -2,6 +2,8 @@
 
 namespace Brooter\AdminBundle\Entity;
 
+use Brooter\PropertyBundle\Entity\PostProperty;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -38,6 +40,18 @@ class PropSubCate
     private $propertyCategory;
     // ...
 
+
+
+    /**
+     * @ORM\OneToMany(targetEntity="Brooter\PropertyBundle\Entity\PostProperty", mappedBy="propSubCate",cascade={"persist"})
+     */
+    private $postProperty;
+
+
+    public function __construct() {
+
+        $this->postProperty=new ArrayCollection();
+    }
     /**
      * Get id
      *
@@ -69,6 +83,51 @@ class PropSubCate
      */
     public function getPropSubCateName()
     {
+        return $this->propSubCateName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPostProperty()
+    {
+        return $this->postProperty;
+    }
+    public function addPostProperty(PostProperty $postProperty)
+    {
+        $postProperty->addPropSubCate($this);
+        $this->postProperty->add($postProperty);
+    }
+    public function removePostProperty(PostProperty $postProperty)
+    {
+        $this->postProperty->removeElement($postProperty);
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getPropertyCategory()
+    {
+        return $this->propertyCategory;
+    }
+
+    /**
+     * @param mixed $propertyCategory
+     */
+    public function setPropertyCategory($propertyCategory)
+    {
+        $this->propertyCategory = $propertyCategory;
+    }
+    
+    public function addPropertyCate(PropertyCate $propertyCategory)
+    {
+        $this->setPropertyCategory($propertyCategory);
+    }
+
+    public function __toString()
+    {
+        // TODO: Implement __toString() method.
         return $this->propSubCateName;
     }
 }
